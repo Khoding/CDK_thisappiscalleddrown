6b8d1515f243000cfc9209eaf14640b023de2a01
Khoding <julien.rutscho@ceff.ch>
2020-10-26 08:13:53 +0100

    Modification du model User
    utilisation de AbstractUser, veuillez supprimer votre fichier sqlite, faire les migrations pour accounts et recréer un superuser

--- ceffdevKAPIC/settings.py
+++ ceffdevKAPIC/settings.py
@@ -38,6 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    # Applications Packages / Modification de defaults
+    'accounts',  # Pour changer le model User
+
     # Apps
     'koolapic',
     'koolapicAPI',
@@ -103,6 +106,7 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+AUTH_USER_MODEL = 'accounts.CustomUser'
 
 # Internationalization
 # https://docs.djangoproject.com/en/3.1/topics/i18n/
