e00d7df8b3da88d3d5bbebb6e3925e55dcf46ca4
Lorenzo Di Benedetto <lorenzo.dibenedetto@ceff.ch>
2020-10-27 16:06:05 +0100

    ¯\_(ツ)_/¯

--- koolapic/urls.py
+++ koolapic/urls.py
@@ -1,11 +1,8 @@
 from django.contrib import admin
-<<<<<<< HEAD
 from django.core.exceptions import PermissionDenied
 from django.http import HttpResponse, Http404, HttpResponseServerError
 from django.urls import path
-=======
 from django.urls import include, path
->>>>>>> master
 
 from koolapic.views import IndexView, ActivityListView, ActivityCreateView, ActivityDetailView
 
