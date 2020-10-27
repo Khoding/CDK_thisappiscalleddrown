e6b97c375a68f9cf2f56b08a17b1267982725810
Julien <julien.rutscho@ceff.ch>
2020-10-27 15:59:51 +0100

    ajout de la photo de profil

--- accounts/views.py
+++ accounts/views.py
@@ -1,10 +1,19 @@
 from django.urls import reverse_lazy
-from django.views.generic.edit import CreateView
+from django.views.generic.edit import CreateView, UpdateView
 
-from .forms import CustomUserCreationForm
+from .forms import CustomUserCreationForm, UserChangeForm, CustomUserChangeForm
 
 
 class SignUpView(CreateView):
     form_class = CustomUserCreationForm
     success_url = reverse_lazy('login')
     template_name = 'registration/signup.html'
+
+
+class UserEditView(UpdateView):
+    form_class = CustomUserChangeForm
+    success_url = reverse_lazy('home')
+    template_name = 'registration/edit_profile.html'
+
+    def get_object(self, queryset=True):
+        return self.request.user
--- koolapic/urls.py
+++ koolapic/urls.py
@@ -1,17 +1,14 @@
 from django.contrib import admin
-<<<<<<< HEAD
 from django.core.exceptions import PermissionDenied
 from django.http import HttpResponse, Http404, HttpResponseServerError
 from django.urls import path
-=======
 from django.urls import include, path
->>>>>>> master
 
 from koolapic.views import IndexView
 
 
 urlpatterns = [
-    path('', IndexView.as_view()),
+    path('', IndexView.as_view(), name='home'),
     path('index/', IndexView.as_view()),
     path('home/', IndexView.as_view()),
     path('api/', include('koolapicAPI.urls')),
