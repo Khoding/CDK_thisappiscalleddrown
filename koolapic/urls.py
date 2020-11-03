ab8c35418433704c5b6968d22025a708bc409f66
Julien <julien.rutscho@ceff.ch>
2020-11-03 11:53:32 +0100

    ok cette fois

--- koolapic/urls.py
+++ koolapic/urls.py
@@ -23,9 +23,4 @@ urlpatterns = [
     path('groups/add', GroupCreateView.as_view(), name="add_group"),
     path('groups/<slug:slug>/update/', GroupUpdateView.as_view(), name="update_group"),
     path('groups/<slug:slug>/confirm_delete/', GroupDeleteView.as_view(), name="group_confirm_delete"),
-    path('', IndexView.as_view(), name='index'),
-    path('index/', IndexView.as_view(), name='index'),
-    path('home/', IndexView.as_view(), name='index'),
-    path('signup/', SignUpView.as_view(), name='signup'),
-    path('login/', KoolapicLoginView.as_view(), name='login'),
 ]
