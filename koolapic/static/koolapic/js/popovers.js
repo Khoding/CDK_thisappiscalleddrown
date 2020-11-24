83eb290e8c815c339542f41c714a0f80a3ee3f52
Nicolas Schwab <nicolas.schwab@ceff.ch>
2020-11-24 09:18:01 +0100

    Refactorisation JS + commentaires HTML
    Tri des bibliothèques JavaScript

---
+++ koolapic/static/koolapic/js/popovers.js
@@ -0,0 +1,25 @@
+$(document).ready(function () {
+    /* Fonction utilisée quand la page a fini de charger */
+    $(function () {
+        createPopovers()
+    });
+
+    /* Crée un popover pour chaque bouton qui en a besoin */
+    function createPopovers() {
+        let buttons = $('.tooltip-btn')
+
+        for (let i = 0; i < buttons.length; i++) {
+            let button = buttons[i];
+
+            let tooltip = $('#' + button.dataset.tooltipId)
+            let buttonSelector = '#' + button.id
+            tippy(buttonSelector, {
+                content: tooltip[0].innerHTML,
+                allowHTML: true,
+                trigger: 'click',
+                theme: 'koolapic'
+            })
+            tooltip.remove();
+        }
+    }
+});
