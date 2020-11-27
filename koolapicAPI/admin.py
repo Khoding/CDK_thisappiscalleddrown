679ba773322139e56680d1a28d47885b6e290117
Lorenzo Di Benedetto <lorenzo.dibenedetto@ceff.ch>
2020-11-27 09:59:21 +0100

    Modif temp activity

--- koolapicAPI/admin.py
+++ koolapicAPI/admin.py
@@ -3,10 +3,14 @@ from koolapicAPI.models import Activity, Group, Inscription, Admission
 
 
 class ActivityAdmin(admin.ModelAdmin):
-    prepopulated_fields = {'slug': ('description',)}
+    prepopulated_fields = {'slug': ('name',)}
+
+
+class GroupAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('name',)}
 
 
 admin.site.register(Activity, ActivityAdmin)
-admin.site.register(Group)
+admin.site.register(Group, GroupAdmin)
 admin.site.register(Inscription)
 admin.site.register(Admission)
