d0d93fe783777cbcc7c6de61daa54a5d2ebd5e6b
Lorenzo Di Benedetto <lorenzo.dibenedetto@ceff.ch>
2020-12-17 15:11:19 +0100

    Modification cropper

--- koolapic/static/koolapic/js/crop.js
+++ koolapic/static/koolapic/js/crop.js
@@ -4,11 +4,14 @@ $(document).ready(function () {
     let x, y, width, height;
     let cropper;
     $("#id_profile_pic").change(function () {
+        if (cropper) {
+            cropper.destroy();
+        }
         if (this.files && this.files[0]) {
             let reader = new FileReader();
             reader.onload = function (e) {
-                $("#imageCrop").attr("src", e.target.result);
-                let image = document.getElementById("imageCrop");
+                $("#imageCrop").attr('src', e.target.result);
+                let image = document.getElementById('imageCrop');
                 $("#modalCrop").modal('show');
                 cropper = new Cropper(image, {
                     aspectRatio: 1 / 1,
@@ -25,13 +28,13 @@ $(document).ready(function () {
     });
 
     $("#saveImageCropped").click(function () {
-        cropper.destroy();
-    });
-
-    $("#formUpload").click(function () {
         $("#id_x").val(x);
         $("#id_y").val(y);
         $("#id_width").val(width);
         $("#id_height").val(height);
+    });
+
+    $("#formUpload").click(function () {
+
     });
 });
