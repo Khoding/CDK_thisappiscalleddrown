b1f4d5fdb80d095347c866df684a8c7ec3998426
Lorenzo Di Benedetto <lorenzo.dibenedetto@ceff.ch>
2020-12-22 08:58:24 +0100

    Ajout du crop pour la modification/ajout des groupes

--- koolapic/static/koolapic/js/crop.js
+++ koolapic/static/koolapic/js/crop.js
@@ -32,6 +32,35 @@ $(document).ready(function () {
         }
     });
 
+    $("#id_image").change(function () {
+        if (cropper) {
+            cropper.destroy();
+        }
+        console.log(this)
+        if (this.files && this.files[0]) {
+            let reader = new FileReader();
+            reader.onload = function (e) {
+                $("#imageCrop").attr('src', e.target.result);
+                let image = document.getElementById('imageCrop');
+                let modal = $("#modalCrop");
+                modal.modal('show');
+                cropper = new Cropper(image, {
+                    viewMode: 2,
+                    aspectRatio: 1072 / 272,
+                    minCropBoxWidth: 100,
+                    minCropBoxHeight: 100,
+                    crop(event) {
+                        x = event.detail.x;
+                        y = event.detail.y
+                        width = event.detail.width;
+                        height = event.detail.height;
+                    },
+                });
+            }
+            reader.readAsDataURL(this.files[0]);
+        }
+    });
+
     $("#formUpload").click(function () {
         $("#id_x").val(x);
         $("#id_y").val(y);
--- koolapicAPI/forms.py
+++ koolapicAPI/forms.py
@@ -1,12 +1,24 @@
 from django import forms
-from django.forms import TextInput
+from django.core import validators
+from django.core.validators import FileExtensionValidator
+from django.forms import TextInput, ImageField
 
-from .models import Activity, Admission, Group, Inscription
+from utils.image_utils import crop_image
+from .models import Activity, Group
 
 from bootstrap_datepicker_plus import DateTimePickerInput
 # TODO pas encore fini
 from easy_maps.widgets import AddressWithMapWidget
+
 # TODO pas encore fini
+
+VALID_IMAGE_EXTENSIONS = [
+    'bmp', 'gif', 'png', 'apng', 'jpg', 'jpeg'
+]
+
+
+class ImageCropField(ImageField):
+    default_validators = [validators.validate_image_file_extension, FileExtensionValidator(allowed_extensions=VALID_IMAGE_EXTENSIONS)]
 
 
 class CustomActivityCreationForm(forms.ModelForm):
@@ -44,24 +56,54 @@ class CustomActivityChangeForm(forms.ModelForm):
 
 
 class CustomGroupCreationForm(forms.ModelForm):
+    x = forms.FloatField(widget=forms.HiddenInput, required=False)
+    y = forms.FloatField(widget=forms.HiddenInput, required=False)
+    width = forms.FloatField(widget=forms.HiddenInput, required=False)
+    height = forms.FloatField(widget=forms.HiddenInput, required=False)
+    image = ImageCropField(required=False, label="Image du groupe")
+
     class Meta:
         model = Group
-        fields = "__all__"
-        exclude = ('slug', 'admission')
+        fields = ('name', 'description', 'visibility', 'invitation_policy', 'banner_color', 'image', 'x', 'y', 'width', 'height')
 
         widgets = {
             'banner_color': TextInput(attrs={'type': 'color'}),
-            'date_don': DateTimePickerInput(format='%d/%m/%Y %H:%M'),
         }
+
+    def save(self, *args, **kwargs):
+        custom_group = super(CustomGroupCreationForm, self).save()
+        if self.cleaned_data.get('width'):
+            x = self.cleaned_data.get('x')
+            y = self.cleaned_data.get('y')
+            w = self.cleaned_data.get('width')
+            h = self.cleaned_data.get('height')
+
+            crop_image((x, y, w, h), (100, 100), custom_group.image.path)
+        return custom_group
 
 
 class CustomGroupChangeForm(forms.ModelForm):
+    x = forms.FloatField(widget=forms.HiddenInput, required=False)
+    y = forms.FloatField(widget=forms.HiddenInput, required=False)
+    width = forms.FloatField(widget=forms.HiddenInput, required=False)
+    height = forms.FloatField(widget=forms.HiddenInput, required=False)
+    image = ImageCropField(required=False, label="Image du groupe")
+
     class Meta:
         model = Group
-        fields = '__all__'
-        exclude = ('slug', 'admission')
+        fields = ('name', 'description', 'visibility', 'invitation_policy', 'banner_color', 'image', 'x', 'y', 'width', 'height')
 
         widgets = {
             'banner_color': TextInput(attrs={'type': 'color'}),
-            'date_don': DateTimePickerInput(format='%d/%m/%Y %H:%M'),
         }
+
+    def save(self, *args, **kwargs):
+        custom_group = super(CustomGroupChangeForm, self).save()
+        if self.cleaned_data.get('width'):
+            x = self.cleaned_data.get('x')
+            y = self.cleaned_data.get('y')
+            w = self.cleaned_data.get('width')
+            h = self.cleaned_data.get('height')
+
+            crop_image((x, y, w, h), (100, 100), custom_group.image.path)
+        return custom_group
