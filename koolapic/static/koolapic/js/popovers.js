f5697edd74cb76ad71603df3b69b568e4762254c
AdrienMR <adrienmatthieu.rossier@ceff.ch>
2020-12-24 09:26:57 +0100

    Correction des popovers
    mettre la font car il ne marche pas encore

--- koolapic/static/koolapic/js/popovers.js
+++ koolapic/static/koolapic/js/popovers.js
@@ -3,7 +3,10 @@
    Popper.js
    Tippy.js
  */
+// import koolapic from  "../css/tippy.scss";
+
 $(document).ready(function () {
+    // const template = document.getElementById('id_username_tooltip')
     /* Fonction utilisée quand la page a fini de charger */
     $(function () {
         createPopovers()
@@ -11,20 +14,25 @@ $(document).ready(function () {
 
     /* Crée un popover pour chaque bouton qui en a besoin */
     function createPopovers() {
-        let buttons = $('.tooltip-btn')
-
-        for (let i = 0; i < buttons.length; i++) {
-            let button = buttons[i];
-
-            let tooltip = $('#' + button.dataset.tooltipId)
-            let buttonSelector = '#' + button.id
-            tippy(buttonSelector, {
-                content: tooltip[0].innerHTML,
+        // let buttons = $('.tooltip-btn')
+        // for (let i = 0; i < buttons.length; i++) {
+        //     let button = buttons[i];
+            tippy('.tooltip-btn', {
+                content(reference) {
+                    const id = reference.getAttribute('data-template');
+                    const template = document.getElementById(id);
+                    return template.innerHTML;
+                },
                 allowHTML: true,
+                theme: 'koolapic',
                 trigger: 'click',
-                theme: 'koolapic'
-            })
-            tooltip.remove();
+                // html: 'template'
+                // onShow(instance) {
+                //     instance.popper.hidden = instance.reference.dataset.tippy ? false : true;
+                //     instance.setContent(instance.reference.dataset.tippy);
+                // }
+            });
+            // console.log(test)
         }
-    }
+    // }
 });
