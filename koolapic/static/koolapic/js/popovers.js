2f90fd4a8a9c3112d2614cdb3c98fdfc3258ccc1
Nicolas Schwab <nicolas.schwab@ceff.ch>
2021-01-13 10:55:17 +0100

    Modifs diverses + corrections tooltips et popovers

--- koolapic/static/koolapic/js/popovers.js
+++ koolapic/static/koolapic/js/popovers.js
@@ -3,10 +3,8 @@
    Popper.js
    Tippy.js
  */
-// import koolapic from  "../css/tippy.scss";
 
 $(document).ready(function () {
-    // const template = document.getElementById('id_username_tooltip')
     /* Fonction utilisée quand la page a fini de charger */
     $(function () {
         createPopovers()
@@ -14,25 +12,28 @@ $(document).ready(function () {
 
     /* Crée un popover pour chaque bouton qui en a besoin */
     function createPopovers() {
-        // let buttons = $('.tooltip-btn')
-        // for (let i = 0; i < buttons.length; i++) {
-        //     let button = buttons[i];
-            tippy('.tooltip-btn', {
-                content(reference) {
-                    const id = reference.getAttribute('data-template');
-                    const template = document.getElementById(id);
-                    return template.innerHTML;
-                },
-                allowHTML: true,
-                theme: 'koolapic',
-                trigger: 'click',
-                // html: 'template'
-                // onShow(instance) {
-                //     instance.popper.hidden = instance.reference.dataset.tippy ? false : true;
-                //     instance.setContent(instance.reference.dataset.tippy);
-                // }
-            });
-            // console.log(test)
-        }
-    // }
+        tippy('.popover-btn', {
+            content(reference) {
+                const id = reference.getAttribute('data-template');
+                const template = document.getElementById(id);
+                return template.innerHTML;
+            },
+            allowHTML: true,
+            theme: 'koolapic',
+            trigger: 'click',
+            animation: 'pop'
+        });
+
+        tippy('.tooltip-btn', {
+            content(reference) {
+                const id = reference.getAttribute('data-template');
+                const template = document.getElementById(id);
+                return template.innerHTML;
+            },
+            allowHTML: true,
+            theme: 'koolapic',
+            trigger: 'mouseenter focus',
+            animation: 'pop'
+        });
+    }
 });
