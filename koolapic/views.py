fc033f8642ba9cbea2c48f85769040057f0c5a45
Nicolas Schwab <nicolas.schwab@ceff.ch>
2021-01-19 12:28:08 +0100

    Améliorations groupes

--- koolapic/views.py
+++ koolapic/views.py
@@ -159,6 +159,7 @@ class GroupDetailView(LoginRequiredMixin, DetailView):
         admin_ids = admins.values_list('id', flat=True)
         members = self.object.members.all().exclude(id__in=admin_ids)
         banned_users = self.object.banned_users.all()
+        all_members_count = len(members) + len(admins)
 
         context = super().get_context_data(**kwargs)
         context['title'] = 'Koolapic'
@@ -166,8 +167,9 @@ class GroupDetailView(LoginRequiredMixin, DetailView):
         context['members'] = members
         context['admins'] = admins
         context['banned_users'] = banned_users
-        context['members_count'] = len(members) + len(admins)
-        context['undisplayed_members_count'] = len(members) + len(admins) - 10
+        context['all_members'] = members | admins
+        context['members_count'] = all_members_count
+        context['undisplayed_members_count'] = all_members_count - 10
         return context
 
     def post(self, *args, **kwargs):
