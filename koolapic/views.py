41841901e847085d351ce8c24becc156bcb5d2e2
Nicolas Schwab <nicolas.schwab@ceff.ch>
2021-01-28 12:29:35 +0100

    Fix des invitations pour accepter des nouveaux  users

--- koolapic/views.py
+++ koolapic/views.py
@@ -326,13 +326,13 @@ class InvitationView(DetailView):
         group = self.get_object().group
         if not self.get_object().user or self.request.user == self.get_object().user:
             if self.request.POST.get('accept'):
-                self.get_object().group.members.add(self.get_object().user)
+                self.get_object().group.members.add(self.request.user)
                 self.model.objects.get(id=self.get_object().id).delete()
                 messages.success(request=self.request, message=f"Vous faites désormais partie du groupe {group.name}.")
                 return redirect(reverse('koolapic:group_detail', kwargs={'slug': group.slug}))
             elif self.request.POST.get('decline'):
                 messages.success(request=self.request, message=f"Vous avez décliné l'invitation au groupe {group.name}.")
-                self.model.objects.get(id=self.get_object().id).delete()
+                self.model.objects.get(id=self.request.user.id).delete()
                 return redirect(reverse('koolapic:home'))
         else:
             messages.error(request=self.request, message="Cette invitation ne vous est pas destinée.")
