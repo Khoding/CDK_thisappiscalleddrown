580171ed3ad35bdca85153454df4740834ee6e36
Julien Rutscho <julien.rutscho@ceff.ch>
2021-03-29 11:46:32 +0200

    des contributions de malade
    Ajout de plus de champs aux contributeurs
    champ "aka": Also Known As
    champ "year_when_contr": l'année d'apprentissage quand la personne a contribué au projet
    champ "contribs" les contributions apportées au projet, les contribs ont:
    un nom: la contribution
    une description: un description de la contribution, peut être nulle
    des crédits: actuellement juste des crédits donnés à la main par nous, mais ce serait cool d'essayer de calculer en fonction des commits si c'est possible, sinon on les enlève

--- ceffdevKAPIC/koolapic_settings.py
+++ ceffdevKAPIC/koolapic_settings.py
@@ -11,23 +11,35 @@ CONTRIBUTORS = [
         'name': 'Julien Rutscho',
         'citation': 'I am Vengeance ! I am the Night ! I am Batman !!!',
         'email': 'julien.rutscho@ceff.ch',
+        'year_when_contr': 3,
+        'contribs': [{'name': '"Chef" du projet refonte', 'desc': 'A proposé Django, ce qui a mené à la refonte', 'credits': 75}, {'name': 'Développeur', 'desc': 'A contribué au code', 'credits': 200}, {'name': 'Designer', 'desc': 'A contribué au design', 'credits': 75}, {'name': 'Maniac', 'desc': 'Se plaignait de tout ce qui n\'était pas parfait', 'credits': 100}],
+        'aka': 'Le parfois très efficace... seulement parfois'
     },
     {
         'name': 'Nicolas Schwab',
         'citation': '3 glocks 20 blocks',
         'icon': 'fas fa-leaf',
         'email': 'nicolas.schwab@ceff.ch',
+        'year_when_contr': 4,
+        'contribs': [{'name': 'Plus grand contributeur', 'desc': 'A apporté la contribution la plus conséquente au projet', 'credits': 500}, {'name': 'Designer Principal', 'desc': 'A créé le design', 'credits': 250}, {'name': 'Développeur', 'desc': 'A contribué au code', 'credits': 200}],
+        'aka': 'Le trop efficace'
     },
     {
         'name': 'Lorenzo Di Benedetto',
         'citation': 'Galaxy brain',
         'icon': 'fas fa-brain',
         'email': 'lorenzo.dibenedetto@ceff.ch',
+        'year_when_contr': 4,
+        'contribs': [{'name': 'Développeur', 'desc': 'A contribué au code', 'credits': 200}, {'name': 'Créateur des APIs', 'desc': 'A créé les APIs', 'credits': 250}, {'name': 'Designer', 'desc': 'A contribué au Design', 'credits': 50}],
+        'aka': 'L\'Orakle du Python'
     },
     {
         'name': 'Adrien Matthieu Rossier',
         'citation': 'Toujours avoir le choix LOL',
         'icon': 'fas fa-torii-gate',
         'email': 'adrienmatthieu.rossier@ceff.ch',
+        'year_when_contr': 4,
+        'contribs': [{'name': 'Migrations des données depuis l\'ancien site', 'desc': 'A créé le script de migration (2x)', 'credits': 250}, {'name': 'Développeur', 'desc': 'A contribué au code', 'credits': 100}],
+        'aka': 'La belle aux bois dormant'
     }
 ]
